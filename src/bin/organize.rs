9f12de940ba56278c3eec43449dd5663f284e1e4
simonsan <14062932+simonsan@users.noreply.github.com>
2023-05-19 14:57:15 +0200

    Research dependencies

--- src/bin/organize.rs
+++ src/bin/organize.rs
@@ -3,9 +3,11 @@
 #![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
 
+use human_panic::setup_panic;
 use organize_rs::application::ORGANIZE_APP;
 
 /// Boot Organize
 fn main() {
+    setup_panic!();
     abscissa_core::boot(&ORGANIZE_APP);
 }
