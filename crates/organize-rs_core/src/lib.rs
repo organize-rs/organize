398edb29916398c43b15531779233eda4b28eef5
simonsan <14062932+simonsan@users.noreply.github.com>
2023-05-23 21:40:49 +0200

    Fix borrow issues

--- crates/organize-rs_core/src/lib.rs
+++ crates/organize-rs_core/src/lib.rs
@@ -6,11 +6,12 @@ pub mod rules;
 use crate::{
     error::OrganizeResult,
     locations::{MaxDepth, OrganizeLocation, OrganizeTarget},
+    rules::filters::OrganizeFilter,
 };
 
-use std::{fs::FileType, path::Path, vec};
+use std::{fs::FileType, path::Path};
 
-use itertools::{enumerate, Itertools};
+use itertools::Itertools;
 use walkdir::{DirEntry, WalkDir};
 
 pub mod constants {
@@ -21,30 +22,50 @@ pub struct IterCarry<'it> {
     pub iter: &'it mut dyn Iterator<Item = DirEntry>,
 }
 
-#[derive(Debug)]
-pub struct FilterWalker {}
+#[derive(Debug, Clone, Default)]
+pub struct FilterWalker {
+    entries: Vec<DirEntry>,
+}
 
 impl FilterWalker {
-    pub fn get_applicable_items<'carry, 'iter>(
-        locations: &'iter mut impl Iterator<Item = OrganizeLocation>,
-    ) -> Vec<DirEntry> {
-        locations
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn entries(&self) -> &[DirEntry] {
+        &self.entries
+    }
+
+    pub fn get_applicable_items(
+        &mut self,
+        locations: Vec<OrganizeLocation>,
+        filters: Vec<OrganizeFilter>,
+    ) {
+        self.entries = locations
+            .into_iter()
             .map(|location| match location {
                 OrganizeLocation::RecursiveWithMaxDepth {
                     path,
                     max_depth,
                     target,
-                } => Self::entries(path, max_depth, target),
+                } => Self::populate_entries(path, max_depth, target),
                 OrganizeLocation::NonRecursive { path, target } => {
-                    Self::entries(path, None, target)
+                    Self::populate_entries(path, None, target)
                 }
             })
             .flatten_ok()
             .filter_map(std::result::Result::ok)
-            .collect_vec()
+            .filter_map(|entry| {
+                filters
+                    .iter()
+                    .map(|filter| filter.get_filter()(&entry))
+                    .contains(&true)
+                    .then_some(entry)
+            })
+            .collect_vec();
     }
 
-    pub fn entries<A>(
+    fn populate_entries<A>(
         path: A,
         max_depth: impl Into<Option<MaxDepth>>,
         targets: OrganizeTarget,
@@ -60,39 +81,42 @@ impl FilterWalker {
 
         let files: Vec<DirEntry> = WalkDir::new(path)
             .max_depth(depth)
-            .contents_first(true)
+            // .contents_first(true) // turned of due to usage of `filter_entry`
             .into_iter()
-            .filter_map(|f| f.ok())
-            .filter(|f| match targets {
+            .filter_entry(|f| match targets {
                 OrganizeTarget::Dirs => FileType::is_dir(&f.file_type()),
                 OrganizeTarget::Files => FileType::is_file(&f.file_type()),
                 OrganizeTarget::Both => true,
             })
+            .filter_map(|f| f.ok())
             .collect();
 
         Ok(files)
     }
 
-    pub fn apply_single_filter(
-        entries: &mut impl Iterator<Item = DirEntry>,
-        filter: Box<dyn FnMut(&DirEntry) -> bool>,
-    ) -> Vec<DirEntry> {
-        entries.into_iter().filter(filter).collect_vec()
-    }
+    // pub fn apply_single_filter(
+    //     entries: &DirEntry,
+    //     mut filter: Box<dyn FnMut(&DirEntry) -> bool>,
+    // ) -> Vec<DirEntry> {
+    //     entries
+    //         .into_iter()
+    //         .filter_map(|f| filter(&f).then_some(f))
+    //         .collect_vec()
+    // }
 
     pub fn apply_filters(
         entries: Vec<DirEntry>,
-        filters: Vec<Box<dyn FnMut(&DirEntry) -> bool>>,
+        filters: &mut [Box<dyn FnMut(&DirEntry) -> bool>],
     ) -> Vec<DirEntry> {
-        let mut vec = vec![];
-        filters.into_iter().enumerate().for_each(|(idx, filter)| {
-            vec = if idx == 1 {
-                Self::apply_single_filter(&mut entries.clone().into_iter(), filter)
-            } else {
-                Self::apply_single_filter(&mut vec.clone().into_iter(), filter)
-            };
-        });
-
-        vec
+        entries
+            .into_iter()
+            .filter(|entry| {
+                let mut results = vec![];
+                filters
+                    .iter_mut()
+                    .for_each(|filter| results.push(filter(entry)));
+                results.contains(&true)
+            })
+            .collect_vec()
     }
 }
